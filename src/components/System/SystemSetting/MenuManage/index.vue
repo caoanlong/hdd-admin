d59e91487786e5054e929844f5bb94807fc4eeee
coolia <gj@vfu56.com>
2018-01-19 16:08:59 +0800

    修改路由

---
+++ src/components/System/SystemSetting/MenuManage/index.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="main-content">
+		<el-card class="box-card">
+		  <div slot="header" class="clearfix">
+		    <span>菜单列表</span>
+		  </div>
+		  <div class="tableControl">
+		  	<el-button type="default" size="mini" icon="el-icon-plus" @click="jump('/addmenu')">添加</el-button>
+		  	<el-button type="default" size="mini" icon="el-icon-edit">修改</el-button>
+		  	<el-button type="default" size="mini" icon="el-icon-delete">删除</el-button>
+		  	<el-button type="default" size="mini"><svg-icon icon-class="save_icon"></svg-icon> 保存排序</el-button>
+		  	<el-button type="default" size="mini" icon="el-icon-refresh">刷新</el-button>
+		  </div>
+		  <div class="table">
+		  	<el-table :data="tableData" border style="width: 100%" size="mini">
+			    <el-table-column  type="selection" align="center">
+			    </el-table-column>
+			    <el-table-column  label="名称" prop="name">
+			    </el-table-column>
+			    <el-table-column  label="链接" prop="url">
+			    </el-table-column>
+			    <el-table-column  label="排序" prop="sort" width="120" align="center">
+			    </el-table-column>
+			    <el-table-column  label="可见" prop="visibility" width="120" align="center">
+			    </el-table-column>
+			    <el-table-column  label="权限标识">
+			    </el-table-column>
+			    <el-table-column label="操作">
+			    	<template slot-scope="scope">
+						<el-button-group>
+							<el-button type="default" size="mini" icon="el-icon-view"></el-button><el-button type="primary" size="mini" icon="el-icon-edit" title></el-button><el-button type="default" size="mini" icon="el-icon-delete"></el-button><el-button type="danger" size="mini" icon="el-icon-plus"></el-button>
+						</el-button-group>
+				    </template>
+			    </el-table-column>
+			  </el-table>
+			  <div class="pagination">
+			  	<el-pagination background layout="prev, pager, next" :total="500"></el-pagination>
+			  </div>
+		  </div>
+		</el-card>
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data() {
+	      	return {
+		        tableData: [{url: '2016-05-02',
+		          name: '无车承运人',
+		          url:'/home',
+		          sort: '1',
+		          visibility:'可见',
+		          permission:''
+		        }]
+			}
+	    },
+		methods: {
+			handleEdit(index, row) {
+		        console.log(index, row);
+			},
+			handleDelete(index, row) {
+				console.log(index, row);
+			},
+			jump(to){
+				if (this.$router) {
+			        this.$router.push(to)
+			      }
+			}
+		
+		}
+	}
+</script>
+<style lang="stylus">
+	.main-content
+		padding 20px
+		.tableControl
+			margin-bottom 10px
+		.pagination
+			margin-top 10px
+			text-align center
+</style>
---
+++ src/components/System/SystemSetting/RoleManage/index.vue
@@ -0,0 +1,97 @@
+<template>
+	<div class="main-content">
+		<el-card class="box-card">
+		  <div slot="header" class="clearfix">
+			<span>角色列表</span>
+		  </div>
+		  <div class="search">
+			<el-form :inline="true"  class="demo-form-inline"  size="small">
+			  <el-form-item label="角色名称：">
+				<el-input  placeholder="角色名称"></el-input>
+			  </el-form-item>
+			  <el-form-item>
+				<el-button type="primary">查询</el-button>
+				<el-button type="default">重置</el-button>
+			  </el-form-item>
+			</el-form>
+		  </div>
+		  <div class="tableControl">
+			<el-button type="default" size="mini" icon="el-icon-plus">添加</el-button>
+			<el-button type="default" size="mini" icon="el-icon-edit">修改</el-button>
+			<el-button type="default" size="mini" icon="el-icon-delete">添加</el-button>
+			<el-button type="default" size="mini" icon="el-icon-refresh">刷新</el-button>
+		  </div>
+		  <div class="table">
+			<el-table :data="tableData" border style="width: 100%" size="mini">
+				<el-table-column type="selection" align="center">
+				</el-table-column>
+				<el-table-column label="角色名称" prop="roleName">
+				</el-table-column>
+				<el-table-column label="英文名称" prop="name">
+				</el-table-column>
+				<el-table-column label="归属机构" prop="affiliation">
+				</el-table-column>
+				<el-table-column label="数据范围" prop="dataArea">
+				</el-table-column>
+				<el-table-column label="操作">
+					<template slot-scope="scope">
+						<el-button-group>
+						<el-button type="default" size="mini" icon="el-icon-view"></el-button>
+						  <el-button type="primary" size="mini" icon="el-icon-edit" title></el-button>
+						  <el-button type="danger" size="mini" icon="el-icon-delete"></el-button>
+						  <el-button type="default" size="mini" icon="el-icon-setting"></el-button>
+						  <el-button type="default" size="mini" icon="el-icon-plus"></el-button>
+						</el-button-group>
+					</template>
+				</el-table-column>
+			  </el-table>
+			  <div class="pagination">
+			  <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+			  </div>
+		  </div>
+		</el-card>
+
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data() {
+			return {
+			tableData: [{
+				id:'1',
+				roleName:'管理员',
+				name: 'Admin',
+				affiliation: '',
+				dataArea: '所有数据'
+			},{
+				id:'2',
+				roleName:'客服人员',
+				name: 'Customer Service',
+				affiliation: '总公司',
+				dataArea: '所有数据'
+			},{
+				id:'3',
+				roleName:'系统管理员',
+				name: 'Administrator',
+				affiliation: '总公司',
+				dataArea: '所有数据'
+			},{
+				id:'4',
+				roleName:'运维管理员',
+				name: 'Operations Manager',
+				affiliation: '总公司',
+				dataArea: '所有数据'
+			}]
+		}
+	}
+}
+</script>
+<style lang="stylus">
+	.main-content
+		padding 20px
+		.tableControl
+			margin-bottom 10px
+		.pagination
+			margin-top 10px
+			text-align center
+</style>
