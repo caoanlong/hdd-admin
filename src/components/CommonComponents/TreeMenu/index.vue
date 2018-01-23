b70e0684c2286b8d97c4f51a464c58dc8f3e0357
Caoanlong <caoanlong@gmail.com>
2018-01-23 18:17:36 +0800

    树菜单

---
+++ src/components/CommonComponents/TreeMenu/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<scroll-bar>
+		<tree-menu-item :routes="routes"></tree-menu-item>
+	</scroll-bar>
+</template>
+<script type="text/javascript">
+import ScrollBar from '../ScrollBar'
+import TreeMenuItem from './TreeMenuItem'
+export default {
+	name: 'tree-menu',
+	props: {
+		routes: {
+			type: Array,
+			default: () => [
+				{
+					"path": "/",
+					"name": "home",
+					"component": "/Home",
+					"meta": {
+						"title": "首页",
+						"icon": "home-icon",
+						"roles": "admin,editor",
+						"parent": null,
+						"isMenu": true
+					},
+					"redirect": null,
+					"children": null
+				}
+			]
+		}
+	},
+	methods: {
+	},
+	components: { 
+		ScrollBar,
+		TreeMenuItem
+	}
+}
+</script>
