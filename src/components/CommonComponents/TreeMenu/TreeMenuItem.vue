4b9fde0fcf7c57b8bd26acf77d1edea13d517e89
Caoanlong <caoanlong@gmail.com>
2018-01-24 11:46:54 +0800

    gg

--- src/components/CommonComponents/TreeMenu/TreeMenuItem.vue
+++ src/components/CommonComponents/TreeMenu/TreeMenuItem.vue
@@ -1,21 +1,13 @@
 <template>
 	<div class="tree-menu-wrapper">
-		<div v-for="item in routes" v-if="item.meta.isMenu">
-			<div class="tree-menu-title" :class="{'active': selectedOption.name == item.name}" @click.stop="selectOption(item)">
+		<div v-for="item in routes">
+			<div class="tree-menu-title" @click.stop="selectOption(item)">
 				<el-checkbox></el-checkbox>
 				<svg-icon v-if="item.meta.icon" :icon-class="item.meta.icon"></svg-icon>
 				<span slot="title">{{item.meta.title}}</span>
+				<span v-if="item.children">[{{isOpen ? '-' : '+'}}]</span>
 			</div>
-			<div v-show="selectedOption.isCollapse">
-				<div style="padding-left: 20px" v-for="child in item.children" v-if="child.meta.isMenu">
-					<tree-menu-item v-if="child.children && child.children.length > 0" :routes="[child]"></tree-menu-item>
-					<div class="tree-menu-title" :class="{'active': selectedOption.name == child.name}" @click.stop="selectOption(child)" v-else>
-						<el-checkbox></el-checkbox>
-						<svg-icon v-if="child.meta.icon" :icon-class="child.meta.icon"></svg-icon>
-						<span>{{child.meta.title}}</span>
-					</div>
-				</div>
-			</div>
+			<tree-menu-item :routes="item.children" v-show="isOpen" v-if="item.children"/>
 		</div>
 	</div>
 </template>
@@ -26,26 +18,24 @@ export default {
 	props: {
 		routes: {
 			type: Array
-		},
-		isNest: {
-			type: Boolean,
-			default: false
 		}
 	},
 	data() {
 		return {
-			selectedOption: {}
+			selectedOption: null,
+			isOpen: false
+		}
+	},
+	computed: {
+		isFolder() {
+			return 
 		}
 	},
 	methods: {
 		selectOption(item) {
 			this.selectedOption = item
-			if (this.selectedOption.isCollapse) {
-				this.selectedOption.isCollapse = false
-			} else {
-				this.selectedOption.isCollapse = true
-			}
-			console.log(JSON.stringify(item.meta.title), this.selectedOption.isCollapse)
+			this.isOpen = !this.isOpen
+			console.log(item.meta.title)
 		}
 	}
 }
@@ -59,5 +49,7 @@ export default {
 		&.active
 			color #fff
 			background #409eff
+	.tree-menu-wrapper
+		padding-left 15px
 </style>
 
