4c0b7f654ff6d56e883a0eae18c144e4b9ef2e9d
Caoanlong <caoanlong@gmail.com>
2018-01-31 15:57:09 +0800

    gg

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
 	NODE_ENV: '"development"',
-	BASE_API: '"http://192.168.1.48:3000/hddadmin"'
+	BASE_API: '"http://192.168.1.16:3000/hddadmin"'
 })
