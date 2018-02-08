ddb1c130ea2fff152dfe47d8fc6b764ddd87a185
coolia <gj@vfu56.com>
2018-02-08 12:23:32 +0800

    还原~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- src/components/System/DataManage/ConstManage/index.vue
+++ src/components/System/DataManage/ConstManage/index.vue
@@ -1,13 +1,293 @@
 <template>
-	<div>
-		<router-view></router-view>
+	<div class="main-content">
+		<el-card class="box-card">
+			<div slot="header" class="clearfix">
+				<span>基础常量列表</span>
+			</div>
+			<div class="search">
+				<el-form :inline="true" class="form-inline" size="small">
+					<el-form-item label="常量类型" >
+						<el-select placeholder="请选择" v-model="selectedConstantType" class="constantSelect">
+							<el-option v-for="item in ConstantTypeList" :key="item.VALUE" :label="item.VALUE" :value="item.VALUE">
+							</el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="名称">
+						<el-input placeholder="名称"></el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-button type="primary" @click.native="getConstants">查询</el-button>
+						<el-button type="default" @click.native="reset">重置</el-button>
+					</el-form-item>
+				</el-form>
+			</div>
+			<div class="tableControl">
+				<el-button type="default" size="mini" icon="el-icon-plus" @click.native="AddConstant">添加</el-button>
+				<el-button type="default" size="mini" icon="el-icon-delete" @click.native="deleteConfirm">批量删除</el-button>
+				<upload-excel btnType="default" btnTxt="导入" @on-selected-file="onSelectedFile"/>
+				<el-button type="default" size="mini" icon="el-icon-download" :loading="downloadLoading" @click.native="exportExcel">导出</el-button>
+				<a href="../../../../../static/constant_template.xlsx" download="constant_template.xlsx" class="download-btn"><svg-icon iconClass="excel-icon"></svg-icon> 下载模板</a>
+				<el-button type="default" size="mini" icon="el-icon-refresh" :loading="refreshing" @click.native="refresh">刷新</el-button>
+				
+			</div>
+			<div class="table">
+				<el-table :data="constants" @selection-change="selectionChange" border style="width: 100%" size="mini">
+					<el-table-column label="Id" type="selection" align="center" width="40"></el-table-column>
+					<el-table-column label="常量类型" prop="Type"></el-table-column>
+					<el-table-column label="代码" prop="Code"></el-table-column>
+					<el-table-column label="名称" prop="Name"></el-table-column>
+					<el-table-column label="值" prop="Value"></el-table-column>
+					<el-table-column label="描述" prop="Description"></el-table-column>
+					<el-table-column label="排序" prop="SortNumber" width="60" align="center"></el-table-column>
+					<el-table-column label="更新人" prop="UpdateBy"width="100" align="center"></el-table-column>
+					<el-table-column label="更新日期" align="center" width="140">
+						<template slot-scope="scope">
+							<span>{{ new Date(scope.row.UpdateTime).getTime() | getdatefromtimestamp() }}</span>
+						</template>
+					</el-table-column>
+					<el-table-column label="操作" width="230" align="center">
+						<template slot-scope="scope">
+							<el-button size="mini" icon="el-icon-view" @click="viewConstant(scope.row.ConstStd_ID)">查看</el-button>
+							<el-button size="mini" icon="el-icon-edit" @click="editConstant(scope.row.ConstStd_ID)">编辑</el-button>
+							<el-button size="mini" icon="el-icon-delete" @click="deleteConfirm(scope.row.ConstStd_ID)">删除</el-button>
+						</template>
+					</el-table-column>
+				</el-table>
+				<div class="pagination">
+					<el-pagination background layout="prev, pager, next" :total="count" @current-change="pageChange"></el-pagination>
+				</div>
+			</div>
+		</el-card>
 	</div>
 </template>
 <script type="text/javascript">
-	export default {
-		
+import request from '../../../../common/request'
+import { Message } from 'element-ui'
+import UploadExcel from '../../../CommonComponents/UploadExcel'
+const userMap = {
+	'常量类型':'Type',
+	'代码':'Code',
+	'名称':'Name',
+	'值':'Value',
+	'描述':'Description',
+	'排序':'SortNumber'
+}
+export default {
+	data() {
+		return {
+			downloadLoading: false,
+			refreshing: false,
+			filename: '基础常量',
+			constants: [],
+			pageIndex: 1,
+			pageSize: 10,
+			findName: '',
+			findUsername: '',
+			findCompany: '',
+			findDepartment: '',
+			count: 0,
+			selectedConstants: [],
+			selectedConstantType:'',
+			ConstantTypeList:[]
+		}
+	},
+	created() {
+		this.getConstants()
+		this.getConstantType()
+	},
+	methods: {
+		exportExcel() {
+			this.downloadLoading = true
+			import('../../../../common/Export2Excel').then(excel => {
+				const tHeader = ['常量类型','代码','名称','值','描述','排序']
+				const filterVal = [userMap['常量类型'], userMap['代码'], userMap['名称'], userMap['值'], userMap['描述'], userMap['排序']]
+				const data = this.formatJson(filterVal, this.constants)
+				excel.export_json_to_excel(tHeader, data, this.filename)
+				this.downloadLoading = false
+			})
+		},
+		formatJson(filterVal, jsonData) {
+			return jsonData.map(v => filterVal.map(j => {
+				if (j === 'timestamp') {
+					return parseTime(v[j])
+				} else {
+					return v[j]
+				}
+			}))
+		},
+		onSelectedFile(result) {
+			new Promise((resolve, reject) => {
+				let uploadExcelConstants = []
+				result.forEach(item => {
+					let excelConstant = {}
+					for (let key in item) {
+						excelConstant[userMap[key]] = item[key]
+					}
+					uploadExcelConstants.push(excelConstant)
+				})
+				resolve(uploadExcelConstants)
+			}).then(constants => {
+				this.addUserMutiple(constants)
+			})
+		},
+		addUserMutiple(consts) {
+			let data = {
+				consts: consts,
+			}
+			request({
+				url: '/base_conststand/addmutip',
+				method: 'post',
+				data
+			}).then(res => {
+				if (res.data.code == 0) {
+					// console.log(res.data)
+					Message.success(res.data.msg)
+					this.getConstants()
+				} else {
+					Message.error(res.data.msg)
+				}
+			})
+		},
+		pageChange(index) {
+			this.getConstants(index)
+		},
+		// 重置搜索表单
+		reset() {
+			this.findName = ''
+		},
+		getConstantType(){
+			let params = {
+				TYPE:'base_constsand'
+			}
+			request({
+				url: '/sys_dict/list/type',
+				method: 'get',
+				params
+			}).then(res => {
+				if (res.data.code == 0) {
+					this.ConstantTypeList = res.data.data
+					this.setRouteQuery({
+						pageIndex: res.data.data.pageIndex,
+						pageSize: res.data.data.pageSize,
+					})
+				} else {
+					Message.error(res.data.msg)
+				}
+			})
+		},
+		getConstants(pageIndex) {
+			let params = {
+				pageIndex: pageIndex || this.$route.query.pageIndex || 1,
+				pageSize: this.$route.query.pageSize || this.pageSize,
+				Type: this.selectedConstantType
+			}
+			request({
+				url: '/base_conststand/list',
+				method: 'get',
+				params
+			}).then(res => {
+				if (res.data.code == 0) {
+					this.count = res.data.data.count
+					this.constants = res.data.data.rows
+					this.setRouteQuery({
+						pageIndex: res.data.data.pageIndex,
+						pageSize: res.data.data.pageSize,
+					})
+				} else {
+					Message.error(res.data.msg)
+				}
+			})
+		},
+		AddConstant() {
+			this.$router.push({ name: 'addconstant' })
+		},
+		deleteConfirm(id) {
+			let ids = []
+			if (id && typeof id == 'string') {
+				ids = [].concat(id)
+			} else {
+				ids = this.selectedConstants
+			}
+			this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+				confirmButtonText: '确定',
+				cancelButtonText: '取消',
+				type: 'warning'
+			}).then(() => {
+				this.delConstants(ids)
+				this.$message({
+					type: 'success',
+					message: '删除成功!'
+				})
+			}).catch(() => {
+				this.$message({
+					type: 'info',
+					message: '已取消删除'
+				})
+			})
+		},
+		delConstants(ids) {
+			let data = {
+				ids: ids
+			}
+			request({
+				url: '/base_conststand/delete',
+				method: 'post',
+				data
+			}).then(res => {
+				if (res.data.code == 0) {
+					this.getConstants()
+				} else {
+					Message.error(res.data.msg)
+				}
+			})
+		},
+
+		editConstant(id) {
+			this.$router.push({ name: 'editconstant', query: { ConstStd_ID: id} })
+		},
+		viewConstant(id) {
+			this.$router.push({ name: 'viewconstant', query: { ConstStd_ID: id} })
+		},
+		selectionChange(data) {
+			this.selectedConstants = data.map(item => item.ConstStd_ID)
+			console.log(this.selectedConstants)
+		},
+		refresh() {
+			this.refreshing = true
+			this.getConstants()
+			setTimeout(() => {
+				this.refreshing = false
+			}, 500)
+		},
+		setRouteQuery(json) {
+			for (let attr in json) {
+				this.$route.query[attr] = json[attr]
+			}
+		}
+	},
+	components: {
+		UploadExcel
 	}
+}
+
 </script>
 <style lang="stylus">
-	
+.download-btn
+	font-size 12px
+	color #606266
+	height 29px
+	line-height 29px
+	padding 0 15px
+	border 1px solid #dcdfe6
+	border-radius 3px
+	background #fff
+	margin 0 10px
+	display inline-block
+	&:hover
+		border-color #c6e2ff
+		color #409eff
+		background #ecf5ff
+	&:active
+		border-color #3a8ee6
+		color #3a8ee6
 </style>
--- src/components/System/SystemSetting/RoleManage/index.vue
+++ src/components/System/SystemSetting/RoleManage/index.vue
@@ -1,13 +1,343 @@
 <template>
-	<div>
-		<router-view></router-view>
+	<div class="main-content">
+		<el-card class="box-card">
+			<div slot="header" class="clearfix">
+				<span>角色列表</span>
+			</div>
+			<div class="search">
+				<el-form :inline="true"  class="demo-form-inline"  size="small">
+					<el-form-item label="角色名称：">
+						<el-input placeholder="角色名称" v-model="findRoleName"></el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-button type="primary" @click.native="getRoles">查询</el-button>
+						<el-button type="default" @click.native="reset">重置</el-button>
+					</el-form-item>
+				</el-form>
+			</div>
+			<div class="tableControl">
+				<el-button type="default" size="mini" icon="el-icon-plus" @click.native="addRole">添加</el-button>
+				<el-button type="default" size="mini" icon="el-icon-delete" @click.native="deleteConfirm">批量删除</el-button>
+				<el-button type="default" size="mini" icon="el-icon-refresh" :loading="refreshing" @click.native="refresh">刷新</el-button>
+			</div>
+			<div class="table">
+				<el-table 
+					ref="roleTable" 
+					:data="roles" 
+					@selection-change="selectRoleChange" 
+					border style="width: 100%" size="mini">
+					<el-table-column type="selection" align="center" width="40">
+					</el-table-column>
+					<el-table-column label="角色名称" prop="Name" align="left">
+					</el-table-column>
+					<el-table-column label="英文名称" prop="EnName" align="left">
+					</el-table-column>
+					<el-table-column label="归属机构" prop="organization" align="left">
+					</el-table-column>
+					<el-table-column label="数据范围" prop="DataScope" align="left">
+					</el-table-column>
+					<el-table-column label="操作" width="420" align="center">
+						<template slot-scope="scope">
+							<el-button type="default" size="mini" icon="el-icon-view" @click="viewRole(scope.row.Role_ID)">查看</el-button>
+							<el-button type="default" size="mini" icon="el-icon-edit" @click="editRole(scope.row.Role_ID)">修改</el-button>
+							<el-button type="default" size="mini" icon="el-icon-delete" @click="deleteConfirm(scope.row.Role_ID)">删除</el-button>
+							<el-button type="default" size="mini" icon="el-icon-setting" @click="setAuth(scope.row)">权限设置</el-button>
+							<el-button type="default" size="mini" icon="el-icon-plus"  @click="setUser(scope.row)">分配用户</el-button>
+						</template>
+					</el-table-column>
+				</el-table>
+				<div class="pagination">
+					<el-pagination background layout="prev, pager, next" :total="count" @current-change="pageChange"></el-pagination>
+				</div>
+			</div>
+		</el-card>
+		<el-dialog title="权限设置" :visible.sync="showSetAuth" width="30%">
+			<el-tree
+				:data="menus"
+				show-checkbox
+				default-expand-all
+				node-key="Menu_ID"
+				ref="tree"
+				highlight-current
+				:props="defaultProps"
+				@check-change="selectMenu" style="height:400px;overflow-y:auto">
+			</el-tree>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="showSetAuth = false">取 消</el-button>
+				<el-button type="primary" @click="submitSetAuth">确 定</el-button>
+			</span>
+		</el-dialog>
+		<el-dialog title="分配用户" :visible.sync="showSetUser" width="600px">
+			<el-table 
+				ref="usersTable"
+				:data="users" 
+				height="400"
+				@selection-change="selectUserChange" 
+				border style="width: 100%" 
+				size="mini">
+				<el-table-column type="selection" align="center"></el-table-column>
+				<el-table-column label="登录名" prop="LoginName"></el-table-column>
+				<el-table-column label="姓名" prop="Name"></el-table-column>
+				<el-table-column label="电话" prop="Phone"></el-table-column>
+				<el-table-column label="手机" prop="Mobile"></el-table-column>
+			</el-table>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="showSetUser = false">取 消</el-button>
+				<el-button type="primary" @click="submitSetUser">确 定</el-button>
+			</span>
+		</el-dialog>
 	</div>
 </template>
 <script type="text/javascript">
+	import { mapGetters } from 'vuex'
+	import request from '../../../../common/request'
+	import { Message } from 'element-ui'
 	export default {
-		
+		data() {
+			return {
+				refreshing: false,
+				roles: [],
+				role: {},
+				pageIndex: 1,
+				pageSize: 10,
+				count: 0,
+				findRoleName: '',
+				selectedRoles: [],
+				setAuthId: '',
+				setUserId: '',
+				// 所有的用户
+				users:[],
+				showSetAuth: false,
+				showSetUser:false,
+				defaultProps: {
+					children: 'children',
+					label: 'title'
+				},
+				selectedMenuId: [],
+				selectedUsers:[],
+			}
+		},
+		computed: {
+			...mapGetters([
+				'menus'
+			])
+		},
+		created() {
+			this.getRoles()
+		},
+		methods: {
+			addRole() {
+				this.$router.push({name: 'addrole'})
+			},
+			editRole(id) {
+				this.$router.push({name: 'editrole', query: {Role_ID: id}})
+			},
+			viewRole(id) {
+				this.$router.push({name: 'viewrole', query: {Role_ID: id}})
+			},
+			pageChange(index) {
+				this.getRoles(index)
+			},
+			selectRoleChange(data) {
+				this.selectedRoles = data.map(item => item.Role_ID)
+			},
+			selectUserChange(data) {
+				// console.log(data)
+				this.selectedUsers = data
+			},
+			// 重置搜索表单
+			reset() {
+				this.findRoleName = ''
+			},
+			getRoles(pageIndex) {
+				let params = {
+					pageIndex: pageIndex || this.$route.query.pageIndex || 1,
+					pageSize: this.$route.query.pageSize || this.pageSize,
+					Name: this.findRoleName
+				}
+				request({
+					url: '/sys_role/list',
+					method: 'get',
+					params
+				}).then(res => {
+					if (res.data.code == 0) {
+						this.count = res.data.data.count
+						this.roles = res.data.data.rows
+						this.setRouteQuery({
+							pageIndex: res.data.data.pageIndex,
+							pageSize: res.data.data.pageSize,
+						})
+					} else {
+						Message.error(res.data.msg)
+					}
+				})
+			},
+			deleteConfirm(id) {
+				let ids = []
+				if (id && typeof id == 'string') {
+					ids = [].concat(id)
+				} else {
+					ids = this.selectedRoles
+				}
+				this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+					this.delRole(ids)
+					this.$message({
+						type: 'success',
+						message: '删除成功!'
+					})
+				}).catch(() => {
+					this.$message({
+						type: 'info',
+						message: '已取消删除'
+					})
+				})
+			},
+			delRole(ids) {
+				let data = {
+					ids: ids
+				}
+				request({
+					url: '/sys_role/delete',
+					method: 'post',
+					data
+				}).then(res => {
+					if (res.data.code == 0) {
+						this.getRoles()
+					} else {
+						Message.error(res.data.msg)
+					}
+				})
+			},
+			setAuth(data) {
+				this.setAuthId = data.Role_ID
+				this.showSetAuth = true
+				this.getRole(data.Role_ID, res => {
+					let menusID = res.sys_menus.map(item => item.Menu_ID)
+					this.$nextTick(() => {
+						this.$refs.tree.setCheckedKeys(menusID)
+						this.getRoles()
+					})
+				})
+			},
+			submitSetAuth() {
+				let data = {
+					Role_ID: this.setAuthId,
+					sys_menus: this.selectedMenuId
+				}
+				request({
+					url: '/sys_role/update/menu',
+					method: 'post',
+					data
+				}).then(res => {
+					if (res.data.code == 0) {
+						Message.success(res.data.msg)
+					} else {
+						Message.error(res.data.msg)
+					}
+					this.showSetAuth = false
+				})
+			},
+			selectMenu(data, isSelected) {
+				if (isSelected) {
+					this.selectedMenuId.push(data.Menu_ID)
+				} else {
+					this.selectedMenuId.splice(this.selectedMenuId.indexOf(data.Menu_ID), 1)
+				}
+			},
+			// 获取当前角色详情
+			getRole(Role_ID, callback) {
+				let params = {
+					Role_ID
+				}
+				request({
+					url: '/sys_role/info',
+					method: 'get',
+					params
+				}).then(res => {
+					if (res.data.code == 0) {
+						this.role = res.data.data
+						callback && callback(res.data.data)
+					} else {
+						Message.error(res.data.msg)
+					}
+				})
+			},
+			// 获取所有用户
+			getUsers(callback) {
+				let params = {
+					pageSize: 100
+				}
+				request({
+					url: '/sys_user/list',
+					method: 'get',
+					params
+				}).then(res => {
+					if (res.data.code == 0) {
+						this.users = res.data.data.rows
+						callback && callback()
+					} else {
+						Message.error(res.data.msg)
+					}
+				})
+			},
+			setUser(data) {
+				this.setUserId = data.Role_ID
+				this.showSetUser = true
+				this.getUsers(() => {
+					this.getRole(data.Role_ID, res => {
+						let usersID = res.sys_users.map(item => item.User_ID)
+						let users = this.users.filter(user => {
+							return usersID.includes(user.User_ID)
+						})
+						console.log(users)
+						users.forEach(user => {
+							this.$refs.usersTable.toggleRowSelection(user)
+						})
+						this.getRoles()
+					})
+				})
+			},
+			handleChange(value, direction, movedKeys) {
+				console.log(value, direction, movedKeys)
+			},
+			submitSetUser() {
+				this.showSetUser = false
+				let userIds = this.selectedUsers.map(item => item.User_ID)
+				let data = {
+					Role_ID: this.setUserId,
+					sys_users: userIds
+				}
+				request({
+					url: '/sys_role/update/user',
+					method: 'post',
+					data
+				}).then(res => {
+					if (res.data.code == 0) {
+						Message.success(res.data.msg)
+					} else {
+						Message.error(res.data.msg)
+					}
+				})
+			},
+			refresh() {
+				this.refreshing = true
+				this.getRoles()
+				setTimeout(() => {
+					this.refreshing = false
+				}, 500)
+			},
+			setRouteQuery(json) {
+				for (let attr in json) {
+					this.$route.query[attr] = json[attr]
+				}
+			}
+		}
 	}
 </script>
 <style lang="stylus">
-	
+
 </style>
