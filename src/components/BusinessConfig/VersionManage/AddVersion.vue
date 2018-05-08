d217b6cc8764bd9c3f6bb45ad2f889f02caf9945
coolia <gj@vfu56.com>
2018-05-08 10:16:13 +0800

    ccc

--- src/components/BusinessConfig/VersionManage/AddVersion.vue
+++ src/components/BusinessConfig/VersionManage/AddVersion.vue
@@ -4,9 +4,9 @@
 			<div slot="header" class="clearfix">
 				<span>添加App版本</span>
 			</div>
-			<el-row>
-				<el-col :span="8">
-					<el-form label-width="120px">
+			<el-form label-width="120px">
+				<el-row>
+					<el-col :span="8">
 						<el-form-item label="用户设备">
 							<el-select v-model="Version.deviceType" placeholder="请选择">
 								<el-option label="安卓端" value="Android"></el-option>
@@ -16,10 +16,8 @@
 						<el-form-item label="最低版本号">
 							<el-input v-model="Version.versionMin"></el-input>
 						</el-form-item>
-					</el-form>
-				</el-col>
-				<el-col :span="8">
-					<el-form label-width="120px">
+					</el-col>
+					<el-col :span="8">
 						<el-form-item label="类型">
 							<el-select v-model="Version.type" placeholder="请选择">
 								<el-option label="司机端" value="Driver"></el-option>
@@ -29,90 +27,86 @@
 						<el-form-item label="版本大小">
 							<el-input v-model="Version.versionSize"></el-input>
 						</el-form-item>
-					</el-form>
-				</el-col>
-				<el-col :span="8">
-					<el-form label-width="120px">
+					</el-col>
+					<el-col :span="8">
 						<el-form-item label="主版本号">
 							<el-input v-model="Version.version"></el-input>
 						</el-form-item>
 						<el-form-item label="是否最新版本">
 							<el-switch v-model="Version.isLatest"></el-switch>
 						</el-form-item>
-					</el-form>
-				</el-col>
-				<el-col :span="24">
-					<el-form label-width="120px">		
+					</el-col>
+					<el-col :span="24">
 						<el-form-item label="下载URL">
 							<el-input v-model="Version.downloadURL"></el-input>
 						</el-form-item>
 						<el-form-item label="版本说明">
 							<el-input type="textarea" resize="none" v-model="Version.content"></el-input>
 						</el-form-item>
-					</el-form>
-				</el-col>
-				
-				<el-col :span="24">
-					<el-form label-width="120px">
+					</el-col>
+					<el-col :span="24">
 						<el-form-item>
 							<el-button type="primary" @click="saveVersion">立即保存</el-button>
 							<el-button @click="back">取消</el-button>
 						</el-form-item>
-					</el-form>
-				</el-col>
-			</el-row>
+					</el-col>
+				</el-row>
+			</el-form>
 		</el-card>
 	</div>
 </template>
 <script type="text/javascript">
-	import requestJava from '../../../common/requestJava'
-	import { Message } from 'element-ui'
-	export default {
-		data() {
-			return {
-				Version: {
-					deviceType:'',
-					type:'',
-					versionSize:'',
-					version:'',
-					versionMin:'',
-					isLatest: true,
-					downloadURL:'',
-					content:''
-				}
-			}
-		},
-		methods: {
-			saveVersion() {
-				let data= {
-					deviceType: this.Version.deviceType,
-					type: this.Version.type,
-					versionSize: this.Version.versionSize,
-					version: this.Version.version,
-					versionMin: this.Version.versionMin,
-					isLatest: this.Version.isLatest ? 'Y' : 'N',
-					downloadURL: this.Version.downloadURL,
-					content: this.Version.content
-				}
-				console.log(data)
-				requestJava({
-					url: '/setAppVersion/save',
-					method: 'post',
-					data
-				}).then(res => {
-					if (res.data.code == 200) {
-						Message.success(res.data.message)
-						this.$router.push({name: 'versionmanage'})
-					} else {
-						Message.error(res.data.message)
-					}
-				})
-			},
-			back() {
-				this.$router.go(-1)
+import requestJava from '../../../common/requestJava'
+import { Message } from 'element-ui'
+export default {
+	data() {
+		return {
+			Version: {
+				deviceType: '',
+				type: '',
+				versionSize: '',
+				version: '',
+				versionMin: '',
+				isLatest: true,
+				downloadURL: '',
+				content: ''
 			}
 		}
+	},
+	methods: {
+		saveVersion() {
+			let data = {
+				deviceType: this.Version.deviceType,
+				type: this.Version.type,
+				versionSize: this.Version.versionSize,
+				version: this.Version.version,
+				versionMin: this.Version.versionMin,
+				isLatest: this.Version.isLatest ? 'Y' : 'N',
+				downloadURL: this.Version.downloadURL,
+				content: this.Version.content
+			}
+			console.log(data)
+			requestJava({
+				url: '/setAppVersion/save',
+				method: 'post',
+				data
+			}).then(res => {
+				if (res.data.code == 200) {
+					Message.success(res.data.message)
+					this.$router.push({ name: 'versionmanage' })
+				} else {
+					Message.error(res.data.message)
+				}
+			})
+		},
+		back() {
+			this.$router.go(-1)
+		}
 	}
+}
+
 </script>
 <style lang="stylus" scoped>
+
+
 </style>
--- src/components/BusinessConfig/VersionManage/EditVersion.vue
+++ src/components/BusinessConfig/VersionManage/EditVersion.vue
@@ -4,9 +4,9 @@
 			<div slot="header" class="clearfix">
 				<span>编辑App版本</span>
 			</div>
-			<el-row>
-				<el-col :span="8">
-					<el-form label-width="120px">
+			<el-form label-width="120px">
+				<el-row>
+					<el-col :span="8">
 						<el-form-item label="用户设备">
 							<el-select v-model="Version.deviceType" placeholder="请选择">
 								<el-option label="安卓端" value="Android"></el-option>
@@ -16,10 +16,8 @@
 						<el-form-item label="最低版本号">
 							<el-input v-model="Version.versionMin"></el-input>
 						</el-form-item>
-					</el-form>
-				</el-col>
-				<el-col :span="8">
-					<el-form label-width="120px">
+					</el-col>
+					<el-col :span="8">
 						<el-form-item label="类型">
 							<el-select v-model="Version.type" placeholder="请选择">
 								<el-option label="司机端" value="Driver"></el-option>
@@ -29,37 +27,31 @@
 						<el-form-item label="版本大小">
 							<el-input v-model="Version.versionSize"></el-input>
 						</el-form-item>
-					</el-form>
-				</el-col>
-				<el-col :span="8">
-					<el-form label-width="120px">
+					</el-col>
+					<el-col :span="8">
 						<el-form-item label="主版本号">
 							<el-input v-model="Version.version"></el-input>
 						</el-form-item>
 						<el-form-item label="是否最新版本">
 							<el-switch v-model="Version.isLatest"></el-switch>
 						</el-form-item>
-					</el-form>
-				</el-col>
-				<el-col :span="24">
-					<el-form label-width="120px">
+					</el-col>
+					<el-col :span="24">
 						<el-form-item label="下载URL">
 							<el-input v-model="Version.downloadURL"></el-input>
 						</el-form-item>
 						<el-form-item label="版本说明">
 							<el-input type="textarea" resize="none" v-model="Version.content"></el-input>
 						</el-form-item>
-					</el-form>
-				</el-col>
-				<el-col :span="24">
-					<el-form label-width="120px">
+					</el-col>
+					<el-col :span="24">
 						<el-form-item>
 							<el-button type="primary" @click="saveVersion">立即保存</el-button>
 							<el-button @click="back">取消</el-button>
 						</el-form-item>
-					</el-form>
-				</el-col>
-			</el-row>
+					</el-col>
+				</el-row>
+			</el-form>
 		</el-card>
 	</div>
 </template>
@@ -71,13 +63,13 @@ export default {
 		return {
 			Version: {
 				deviceType: '',
-				type:'',
-				versionSize:'',
-				version:'',
-				versionMin:'',
+				type: '',
+				versionSize: '',
+				version: '',
+				versionMin: '',
 				isLatest: true,
-				downloadURL:'',
-				content:''
+				downloadURL: '',
+				content: ''
 			}
 		}
 	},
@@ -104,7 +96,7 @@ export default {
 			})
 		},
 		saveVersion() {
-			let data= {
+			let data = {
 				appVersionID: this.$route.query.appVersionID,
 				deviceType: this.Version.deviceType,
 				type: this.Version.type,
@@ -123,7 +115,7 @@ export default {
 			}).then(res => {
 				if (res.data.code == 200) {
 					Message.success(res.data.message)
-					this.$router.push({name: 'versionmanage'})
+					this.$router.push({ name: 'versionmanage' })
 				} else {
 					Message.error(res.data.message)
 				}
