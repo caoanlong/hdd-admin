1b677a133573187e8b2bc725e6905b5d2b974063
Caoanlong <caoanlong@gmail.com>
2018-11-01 14:14:35 +0800

    api url

--- config/dev.env.js
+++ config/dev.env.js
@@ -11,7 +11,7 @@ module.exports = merge(prodEnv, {
     // BASE_JAVA_API: '"http://192.168.1.49:9090"',  // 新考
     // BASE_JAVA_API: '"http://192.168.1.43:9898"',  // 飞龙
     // BASE_JAVA_API: '"http://192.168.1.99:8888"',  // 军良
-    BASE_JAVA_API: '"http://192.168.1.48:9898"', // java测试环境
+    BASE_JAVA_API: '"http://192.168.1.48:9898/huaxin"', // java测试环境
     BASE_SYS_API: '"http://192.168.1.29:8080"', // 系统服务
     BASE_IMG_API: '"http://develop.we-service.cn/hdd/image/"' // img测试环境
 })
