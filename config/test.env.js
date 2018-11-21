821a56430be791825fd14cdbe77f453fa028ee67
Caoanlong <caoanlong@gmail.com>
2018-11-21 15:06:34 +0800

    g

--- config/test.env.js
+++ config/test.env.js
@@ -2,7 +2,7 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"test"',
-	BASE_API: '"http://develop.we-service.cn/admin/nodejs/api"',  // nodejs测试环境
+	BASE_API: '"http://develop.we-service.cn/hxadmin/nodejs/api"',  // nodejs测试环境
 	BASE_JAVA_API: '"http://develop.we-service.cn/huaxin/admin/v2"',  // java测试环境
 	BASE_SYS_API: '"http://192.168.1.110:9919"', // 系统服务
 	BASE_IMG_API: '"http://develop.we-service.cn/hx/image/"'  // img测试环境
