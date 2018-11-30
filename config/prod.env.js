2d9370361fda87bb5f130727b9defcd7f4387e7e
Caoanlong <caoanlong@gmail.com>
2018-11-30 17:27:29 +0800

    修改生产环境配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-	BASE_API: '"http://admin.hdd56.com/nodejs/api"', // nodejs生产环境
-	BASE_JAVA_API: '"http://api.hdd56.com/admin"',  // java生产环境
+	BASE_API: '"http://hxadmin.hdd56.com/nodejs"', // nodejs生产环境
+	BASE_JAVA_API: '"http://hxadmin.hdd56.com/api"',  // java生产环境
 	BASE_IMG_API: '"http://image.hdd56.com/"'  // img生产环境
 }
